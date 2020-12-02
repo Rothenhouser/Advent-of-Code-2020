26b605eb79ec01630ae47d5d52d80ebb521c041d
Nicolas Rothenhäusler <rothenhouser@users.noreply.github.com>
2020-12-02 09:05:13 +0100

    Day 2 part 2

--- day2.py
+++ day2.py
@@ -1,17 +1,17 @@
-from typing import Tuple
+from typing import List, Tuple
 
-Rule = Tuple[str, int, int]
+Rule = Tuple[str, List[int]]
 
 
 def parse_rule(rule: str) -> Rule:
     n, letter = rule.split(" ")
-    min_n, max_n = map(int, n.split("-"))
-    return letter, min_n, max_n
+    positions = list(map(int, n.split("-")))
+    return letter, positions
 
 
 def evaluate(rule: Rule, pwd: str) -> bool:
-    letter, min_n, max_n = rule
-    return min_n <= pwd.count(letter) <= max_n
+    letter, positions = rule
+    return sum(pwd[i - 1] == letter for i in positions) == 1
 
 
 def eval_rule(line: str) -> bool:
