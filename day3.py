6472e70a1d2cf1d38574f616a5aa1e96bbb5aaf5
Nicolas Rothenhäusler <rothenhouser@users.noreply.github.com>
2020-12-03 09:29:08 +0100

    Day 3 part 2

--- day3.py
+++ day3.py
@@ -1,12 +1,27 @@
-trees = 0
-pos_from_left = 0
-width = len(".....#....#...#.#..........#...")
+from functools import reduce
 
-with open("day3.in") as f:
-    for l in f.readlines():
-        p = l[pos_from_left]
-        if p == "#":
+field = [list(l.strip()) for l in open("day3.in").readlines()]
+
+
+def count_trees(field, down, right):
+    width = len(field[0])
+    max_down = len(field) - 1
+    trees = 0
+    idown = 0
+    iright = 0
+
+    while idown <= max_down:
+        if field[idown][iright] == "#":
             trees += 1
-        pos_from_left += 3
-        if pos_from_left > width - 1:
-            pos_from_left = pos_from_left - width
+
+        idown += down
+        iright += right
+        if iright > width - 1:
+            iright = iright - width
+
+    return trees
+
+
+rules = ((1, 1), (1, 3), (1, 5), (1, 7), (2, 1))
+
+print(reduce(lambda a, b: a * b, [count_trees(field, *dr) for dr in rules]))
