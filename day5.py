cb80991219de71ff6dd11a18cdad091515adf40c
Nicolas Rothenhäusler <rothenhouser@users.noreply.github.com>
2020-12-09 13:55:46 +0100

    Day 5 part 2

--- day5.py
+++ day5.py
@@ -6,16 +6,43 @@ C = {
 }
 
 
-def parse_code(code):
-    code_as_bin = "".join(str(C[l]) for l in code)
-    row, column = int(code_as_bin[:7], 2), int(code_as_bin[7:], 2)
-    return row, column
+def code_as_bin(code):
+    return "".join(str(C[l]) for l in code)
 
 
-def seat_id(row, column):
+def seat_id(code):
+    bin_code = code_as_bin(code)
+    row, column = int(bin_code[:7], 2), int(bin_code[7:], 2)
     return row * 8 + column
 
 
+def ordered_seat_id(code):
+    return int(code_as_bin(code), 2)
+
+
 if __name__ == "__main__":
-    print(seat_id(*parse_code("FBFBBFFRLR")))
-    print(max(seat_id(*parse_code(l.strip())) for l in open("day5.in").readlines()))
+    print(seat_id("FBFBBFFRLR"))
+
+    print(max(seat_id(l.strip()) for l in open("day5.in").readlines()))
+
+    seat_codes = [l.strip() for l in open("day5.in").readlines()]
+
+    ids_to_ordered = {seat_id(c): ordered_seat_id(c) for c in seat_codes}
+
+    max_ordered_seat_id = ordered_seat_id("BBBBBBBRRR")
+    missing_front_passed = False
+
+    all_taken_seats = [ordered_seat_id(c) for c in seat_codes]
+    for expected_seat_id in range(max_ordered_seat_id):
+        if not missing_front_passed:
+            if expected_seat_id in all_taken_seats:
+                missing_front_passed = True
+        else:
+            if expected_seat_id not in all_taken_seats:
+                print("mine")
+                print(expected_seat_id)
+                b = bin(expected_seat_id)
+                print(int(b[2:-3], 2) * 8 + int(b[-3:], 2))
+                # yes they're identical
+
+                break
