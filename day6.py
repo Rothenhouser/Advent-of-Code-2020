981e51d4906d517ddf1877cdbd4e5613083bcfd8
Nicolas Rothenhäusler <rothenhouser@users.noreply.github.com>
2020-12-09 14:14:38 +0100

    Day 6 part 2

--- day6.py
+++ day6.py
@@ -1,2 +1,11 @@
+from functools import reduce
+
 group_answers = open("day6.in").read().replace("\n\n", "|").replace("\n", "").split("|")
 print(sum(map(len, map(set, group_answers))))
+
+print(
+    sum(
+        len(reduce(lambda a, b: a & b, map(set, one_group)))
+        for one_group in [g.split("\n") for g in open("day6.in").read().split("\n\n")]
+    )
+)
