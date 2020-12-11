b1d9f89c822479099752b72f4c6f08e3e4d3317d
Nicolas Rothenhäusler <rothenhouser@users.noreply.github.com>
2020-12-11 17:01:40 +0100

    Day 7 part 2

--- day7.py
+++ day7.py
@@ -1,6 +1,7 @@
 import re
+from collections import defaultdict
 
-rules = """
+example_rules = """
 light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
 bright white bags contain 1 shiny gold bag.
@@ -12,32 +13,33 @@ faded blue bags contain no other bags.
 dotted black bags contain no other bags.
 """
 
-EXAMPLE = rules.strip().split("\n")
+EXAMPLE = example_rules.strip().split("\n")
 FILE_INPUT = open("day7.in").readlines()
 
 GOLD = "shiny gold"
 
 
-def parse_rules_to_flat_dict(rules_strs):
-    contained_in = {}
+def parse_rules_to_flat_dict(rules_strs, inserter):
+    contents = defaultdict(list, {})
     for rule_str in rules_strs:
         # re.match goes only beginning of string, so no ^ necessary
         [outer_bag] = re.match(re.compile(r"(\w* \w*) bags"), rule_str).groups()
         contains = re.findall(re.compile(r"(\d+) (\w+ \w+) bag"), rule_str)
-        for _n, inner_bag in contains:
-            insert(inner_bag, outer_bag, contained_in)
-
-    return contained_in
+        inserter(outer_bag, contains, contents)
+    return contents
 
 
-def insert(inner_bag, outer_bag, contained):
-    if inner_bag in contained:
-        contained[inner_bag].append(outer_bag)
-    else:
-        contained[inner_bag] = [outer_bag]
+def inside_out(outer_bag, inner_bags, contained_in):
+    for _n, inner_bag in inner_bags:
+        contained_in[inner_bag].append(outer_bag)
 
 
-def find_gold(flat_dict):
+def outside_in(outer_bag, inner_bags, containing):
+    containing[outer_bag] = [(int(n), bag) for n, bag in inner_bags]
+
+
+def bags_containing_gold(rules_strs):
+    flat_dict = parse_rules_to_flat_dict(rules_strs, inside_out)
     return _lookup_outer(flat_dict[GOLD], flat_dict)
 
 
@@ -50,6 +52,20 @@ def _lookup_outer(outer_bags, flat_dict):
     return obsc
 
 
+def gold_bag_contains(rules_strs):
+    flat_dict = parse_rules_to_flat_dict(rules_strs, outside_in)
+    return _count_bags_in(flat_dict[GOLD], flat_dict)
+
+
+def _count_bags_in(inner_bags, flat_dict):
+    n_bags = 0
+    for n, bag in inner_bags:
+        n_bags += n
+        n_bags += n * _count_bags_in(flat_dict[bag], flat_dict)
+    return n_bags
+
+
 if __name__ == "__main__":
-    print(len(find_gold(parse_rules_to_flat_dict(EXAMPLE))))
-    print(len(find_gold(parse_rules_to_flat_dict(FILE_INPUT))))
+    # print(len(bags_containing_gold(EXAMPLE)))
+    print(len(bags_containing_gold(FILE_INPUT)))
+    print(gold_bag_contains(FILE_INPUT))
